573f7921c0339633b612ef16951e73b1d86309a2
Piyush-M01 <modipiyush2001@gmail.com>
2022-08-06 14:44:05 +0530

    Modified Navigation stack for obstacle avoidance

--- my_robot/build/catkin_generated/setup_cached.sh
+++ my_robot/build/catkin_generated/setup_cached.sh
@@ -9,7 +9,6 @@
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/piyush/turtle_bot_ws/src/my_robot/build/devel:$CMAKE_PREFIX_PATH"
 export LD_LIBRARY_PATH="/home/piyush/turtle_bot_ws/src/my_robot/build/devel/lib:$LD_LIBRARY_PATH"
-export PATH='/opt/ros/noetic/bin:/home/piyush/.local/bin:/home/piyush/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PKG_CONFIG_PATH="/home/piyush/turtle_bot_ws/src/my_robot/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PYTHONPATH="/home/piyush/turtle_bot_ws/src/my_robot/build/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES='/home/piyush/turtle_bot_ws/src/my_robot/build/devel/share/common-lisp'
